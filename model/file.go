db6d09b171865fbdf8da35e941a6e1049713bc21
Weslen <savi2w@icloud.com>
2023-10-28 20:51:00 -0300

    feat: Rename file path field

--- model/file.go
+++ model/file.go
@@ -4,7 +4,7 @@ import "time"
 
 type File struct {
 	ID        int64      `db:"file_id"`
-	Path      string     `db:"path"`
+	Location  string     `db:"location"`
 	CreatedAt time.Time  `db:"created_at"`
 	UpdatedAt time.Time  `db:"updated_at"`
 	DeletedAt *time.Time `db:"deleted_at"`
