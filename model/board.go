ae3de62b2161fd1c79394fbd24bcf48f753fb110
Weslen <savi2w@icloud.com>
2023-10-28 20:48:52 -0300

    feat: Add board description

--- model/board.go
+++ model/board.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type Board struct {
-	ID              int64      `db:"board_id"`
-	BoardCategoryID int64      `db:"board_category_id"`
-	Name            string     `db:"name"`
-	Slug            string     `db:"slug"`
-	CreatedAt       time.Time  `db:"created_at"`
-	UpdatedAt       time.Time  `db:"updated_at"`
-	DeletedAt       *time.Time `db:"deleted_at"`
+	ID          int64      `db:"board_id"`
+	Name        string     `db:"name"`
+	Slug        string     `db:"slug"`
+	Description string     `db:"description"`
+	CreatedAt   time.Time  `db:"created_at"`
+	UpdatedAt   time.Time  `db:"updated_at"`
+	DeletedAt   *time.Time `db:"deleted_at"`
 }
